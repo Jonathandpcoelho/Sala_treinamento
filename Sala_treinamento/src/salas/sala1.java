bdd55fbebf271bcdcc26f85b8279c747c379d69a
jonat <jonat@DESKTOP-NO1F9D7>
2021-02-27 14:34:07 -0300

    //commit de refresh da classe sala 1/

--- Sala_treinamento/src/salas/sala1.java
+++ Sala_treinamento/src/salas/sala1.java
@@ -1,10 +1,50 @@
 package salas;
 
+import java.util.Scanner;
+
 public class sala1 {
 
+	private static int qntMax;
+
+	public int getQntMax() {
+		return qntMax;
+	}
+
+	public void setQntMax(int qntMax) {
+		this.qntMax = qntMax;
+	}
+	
+
+
+//metodo main para ter uma lotação nesta sala;
+	
 	public static void main(String[] args) {
-         // sala 1 que comporta no maximo 15 alunos.
+
+	
+		Scanner qntExist = new Scanner(System.in);
+		System.out.println("digite quantas pessoas deseja adicionar nesta sala:  " );
+	
+		qntMax = qntExist.nextInt();
+	  
+	    if ((qntMax >= 12)) 
+	    	     System.out.printf("sala lotada, maximo sao 12 pessoas e existe: " + qntMax);
+	    
+	    else     System.out.printf("ainda existe espaço para mais alunos quantidade maxima da sala é 12, e temos "+qntMax);
+	   
+
+	
+	    
+		
 
 	}
+
+		
+		
+	
+		
+	
+
+
+	
 
 }
