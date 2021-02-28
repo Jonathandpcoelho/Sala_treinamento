474cf885d6ff3c925af64d023ecd120d85fcb69b
jonat <jonat@DESKTOP-NO1F9D7>
2021-02-28 13:42:01 -0300

    commit sala cafe...

--- Sala_treinamento/src/pessoa_nome_sobrenome/Pessoa.java
+++ Sala_treinamento/src/pessoa_nome_sobrenome/Pessoa.java
@@ -1,5 +1,6 @@
 package pessoa_nome_sobrenome;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 import javax.swing.JOptionPane;
@@ -10,45 +11,48 @@ public class Pessoa {
 	//Classe para atribuir valor a pessoa;
 	
 		
-		private int id;
-		private String nome;
+		protected static int id;
+		protected static String nome;
 		private String sobreNome;
 		
 		
 		//gets and setters...
 		
-		public int getId() {
-			return id;
-	}
+		public class Vetor {
+			// array para aluno maximo 25;
+			
+			private Pessoa[] Pessoas = new Pessoa[25];
+			public void adiciona(Pessoa aluno) {
 		
-		public void setId(int id) {
-			this.id = id;
-	}
+			}
+			public void adiciona(int posicao, Pessoa aluno) {
 		
-		public String getNome() {
-			return nome;
-	}
+			}
+			public Pessoa pega(int posicao) {
+				return null;
+			
+			}
+			public void remove(int posicao) {
+			
+			}
+			public boolean contem(Pessoa Pessoa) {
+				return false;
+			
+			}
+			public int tamanho() {
+				return id;
+			
+			}
+			public String toString() {
+			return Arrays.toString(Pessoas);
+			
+			}
+			
+			
+			}
 		
-		public void setNome(String nome) {
-			this.nome = nome;
-	}
 		
-		public String getSobreNome() {
-			return sobreNome;
-	}
-		
-		public void setSobreNome(String sobreNome) {
-			this.sobreNome = sobreNome;
-	}
-		
-		
-	
-		
-		
-		
-		
-		
+			}
 
-	}
 
 
